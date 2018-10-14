78b1de51fb986c8ee68193a609252e87ec943775
Shaileh <shai.work1@gmail.com>
2018-10-14 14:43:53 +0300

    adding connect and disconnect hendlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 require('./config/config')
 const path = require('path');
+const http = require('http');
 const express = require('express');
-
+const socketIO = require('socket.io');
 
 var app = express();
 
 app.use(express.static(path.join(__dirname,'/../public')));
 
+var server = http.createServer(app);
+var io = socketIO(server);
 
-app.listen(process.env.PORT, () => {
+io.on('connection',(socket) => {
+  console.log("user connected");
+
+  socket.on('disconnect', () => {
+    console.log("User disconnected from server");
+  });
+});
+
+server.listen(process.env.PORT, () => {
   console.log(`Server is up on port ${process.env.PORT}`);
 });
